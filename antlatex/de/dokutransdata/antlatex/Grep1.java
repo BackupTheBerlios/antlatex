29666496f8d019068c1cc667f4324c3e85a386b3
jaloma <jaloma>
2006-01-09 13:43:38 +0000

    verbose Attribute eingefuehrt

--- antlatex/de/dokutransdata/antlatex/Grep1.java
+++ antlatex/de/dokutransdata/antlatex/Grep1.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) Ian F. Darwin, http://www.darwinsys.com/, 1996-2002.
  * All rights reserved. Software written by Ian F. Darwin and others.
- * $Id: Grep1.java,v 1.1 2005/11/30 09:20:59 jaloma Exp $
+ * $Id: Grep1.java,v 1.2 2006/01/09 13:43:38 jaloma Exp $
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -49,11 +49,13 @@ public class Grep1 {
 	/** The matcher for this pattern */
 	protected Matcher matcher;
 
+	private boolean verbose = false;
+
 	/**
 	 * Main will make a Grep object for the pattern, and run it on all input
 	 * files listed in argv.
 	 */
-	public static int doit(String[] argv) throws Exception {
+	public static int doit(String[] argv, boolean verbose) throws Exception {
 
 		if (argv.length < 1) {
 			System.err.println("Usage: Grep1 pattern [filename]");
@@ -61,7 +63,7 @@ public class Grep1 {
 		}
 		int res = 0;
 		Grep1 pg = new Grep1(argv[0]);
-
+		pg.setVerbose(verbose);
 		if (argv.length == 1) {
 			res = pg.process(new BufferedReader(
 					new InputStreamReader(System.in)), "(standard input)",
@@ -69,7 +71,7 @@ public class Grep1 {
 		} else {
 			for (int i = 1; i < argv.length; i++) {
 				res = pg.process(new BufferedReader(new FileReader(argv[i])),
-						argv[i], true);
+						argv[i], pg.getVerbose());
 			}
 		}
 		return res;
@@ -105,7 +107,9 @@ public class Grep1 {
 					if (printFileName) {
 						System.out.print(fileName + ": ");
 					}
-					System.out.println(inputLine);
+					if (verbose) {
+						System.out.println(inputLine);
+					}
 					res = 1;
 				}
 			}
@@ -114,5 +118,17 @@ public class Grep1 {
 			System.err.println(e);
 		}
 		return res;
+	}
+
+	public boolean getVerbose() {
+		return verbose;
+	}
+
+	/**
+	 * @param verbose
+	 *            The verbose to set.
+	 */
+	public final void setVerbose(boolean verbose) {
+		this.verbose = verbose;
 	}
 }
