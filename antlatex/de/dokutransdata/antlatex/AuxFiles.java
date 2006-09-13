5e0cd8246e6682aecdb6da3b00769ab9b4d6853c
jaloma <jaloma>
2006-09-13 09:39:01 +0000

    Soll als NestedElement Verwendung finden...

---
+++ antlatex/de/dokutransdata/antlatex/AuxFiles.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2006 by DokuTransData, Jürgen A.Lamers
+ * All rights reserved.
+ */
+
+/**
+ * 
+ */
+package de.dokutransdata.antlatex;
+
+import org.apache.tools.ant.types.FileSet;
+
+/**
+ * @author jaloma
+ *
+ */
+public class AuxFiles extends FileSet {
+
+public static final String RCS_ID = "Version @(#) $id: $";
+
+/**
+ * 
+ */
+public AuxFiles() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+/**
+ * @param arg0
+ */
+public AuxFiles(FileSet arg0) {
+	super(arg0);
+	// TODO Auto-generated constructor stub
+}
+}
+
